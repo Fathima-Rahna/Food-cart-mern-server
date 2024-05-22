0d42ad29496dc696e7f0a88d4d34a651dba2e153
unknown <rahnaam1997@gmail.com>
2024-05-22 23:06:16 +0530

    bismillah

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // const PORT = 5001;
@@ -12,15 +13,22 @@ const connectToDB = require('./db');
 
 connectToDB();
 
+
+// app.use(cors());
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
 })
-app.use(express.json())
+
+
+
+
+app.use(express.json());
 app.use('/api',require("./Routes/CreateUser"));
 app.use('/api',require("./Routes/DisplayData"));
 app.use('/api',require("./Routes/OrderData"));
@@ -36,3 +44,76 @@ app.listen(PORT, () => {
 
 
 
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const PORT = process.env.PORT || 5001;
+// const connectToDB = require('./db');
+
+// connectToDB();
+
+// const allowedOrigins = [
+//     'http://localhost:5174',
+//     'https://food-cart-mern-server-5.onrender.com'
+// ];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
+
+// app.use(express.json());
+// app.use('/api',require("./Routes/CreateUser"));
+// app.use('/api',require("./Routes/DisplayData"));
+// app.use('/api',require("./Routes/OrderData"));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Food Cart started at port:${PORT}`);
+// });
+
+
+
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const PORT = process.env.PORT || 5001;
+// const connectToDB = require('./db');
+
+// connectToDB();
+
+// const allowedOrigins = [
+//     'https://food-cart-mern-server-5.onrender.com'
+    
+// ];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }));
+
+// app.use(express.json());
+// app.use('/api', require("./Routes/CreateUser"));
+// app.use('/api', require("./Routes/DisplayData"));
+// app.use('/api', require("./Routes/OrderData"));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Food Cart started at port:${PORT}`);
+// });
