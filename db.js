1ff8386656c8698eea0665c446869f116df8f1ad
unknown <rahnaam1997@gmail.com>
2024-05-23 12:35:13 +0530

    newturn

--- db.js
+++ db.js
@@ -47,24 +47,76 @@
 
 
 
+
+// // const jwt = require('jsonwebtoken');
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+// // const mongoURI = process.env.MONGO_URI;
+// // const jwtSecret = process.env.JWT_SECRET;
+
+// //////////////////////////////
+// console.log(process.env.MONGO_URI);
+// console.log(process.env.JWT_SECRET);
+
+// // const mongoURI = 'mongodb+srv://gofood:rahna@cluster0.fznakuv.mongodb.net/foodmern?retryWrites=true&w=majority&appName=Cluster0';
+// // const mongoose = require('mongoose');
+
+// // const connectToDB = async () => {
+// //   try {
+// //    ` await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });`
+// //     console.log('Connected to MongoDB Atlas');
+// //     // Rest of your code...
+// //   } catch (error) {
+// //     console.error('Connection to MongoDB Atlas failed:', error);
+// //   }
+// // };
+
+// // module.exports = connectToDB;
+
+
+// const connectToDB = async () => {
+//   try {
+//     // await mongoose.connect(mongoURI);
+//     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+//     console.log('Connected to MongoDB Atlas');
+//     const foodItemsCollection = mongoose.connection.db.collection("food_items");
+//     const foodCategoryCollection = mongoose.connection.db.collection("food_category");
+    
+//     const foodItemsPromise = foodItemsCollection.find({}).toArray();
+//     const foodCategoryPromise = foodCategoryCollection.find({}).toArray();
+    
+//     const [foodItems, foodCategory] = await Promise.all([foodItemsPromise, foodCategoryPromise]);
+
+//     global.food_items = foodItems;
+//     global.food_category = foodCategory;
+
+//     console.log('Fetched food items:', foodItems);
+//     console.log('Fetched food categories:', foodCategory);
+//   } catch (error) {
+//     console.error('Connection to MongoDB Atlas failed:', error);
+//   }
+// };
+
+// module.exports = connectToDB;
+
+
+
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
-const jwtSecret = process.env.JWT_SECRET;
-// const mongoURI = 'mongodb+srv://gofood:rahna@cluster0.fznakuv.mongodb.net/foodmern?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectToDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log('Connected to MongoDB Atlas');
+
     const foodItemsCollection = mongoose.connection.db.collection("food_items");
     const foodCategoryCollection = mongoose.connection.db.collection("food_category");
-    
+
     const foodItemsPromise = foodItemsCollection.find({}).toArray();
     const foodCategoryPromise = foodCategoryCollection.find({}).toArray();
-    
+
     const [foodItems, foodCategory] = await Promise.all([foodItemsPromise, foodCategoryPromise]);
 
     global.food_items = foodItems;
@@ -78,6 +130,23 @@ const connectToDB = async () => {
 };
 
 module.exports = connectToDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -2,40 +2,71 @@
 
 
 
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+// // const PORT = 5001;
+
+// const PORT = process.env.PORT || 5001;
+// const connectToDB = require('./db');
+
+// connectToDB();
+
+
+// // app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// })
+
+
+
+
+// app.use(express.json());
+// app.use('/api',require("./Routes/CreateUser"));
+// app.use('/api',require("./Routes/DisplayData"));
+// app.use('/api',require("./Routes/OrderData"));
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+
+// app.listen(PORT, () => {
+//   console.log(`Food Cart started at port:${PORT}`);
+// });
+
+
+
+
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
-// const PORT = 5001;
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5001;
 const connectToDB = require('./db');
 
 connectToDB();
 
-
-// app.use(cors());
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
-
-
-
+app.use(cors({
+  origin: 'http://localhost:5174'  // Replace with your actual frontend URL
+}));
 
 app.use(express.json());
-app.use('/api',require("./Routes/CreateUser"));
-app.use('/api',require("./Routes/DisplayData"));
-app.use('/api',require("./Routes/OrderData"));
+app.use('/api', require("./Routes/CreateUser"));
+app.use('/api', require("./Routes/DisplayData"));
+app.use('/api', require("./Routes/OrderData"));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Food Cart started at port:${PORT}`);
@@ -44,76 +75,3 @@ app.listen(PORT, () => {
 
 
 
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-// const connectToDB = require('./db');
-
-// connectToDB();
-
-// const allowedOrigins = [
-//     'http://localhost:5174',
-//     'https://food-cart-mern-server-5.onrender.com'
-// ];
-
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// }));
-
-// app.use(express.json());
-// app.use('/api',require("./Routes/CreateUser"));
-// app.use('/api',require("./Routes/DisplayData"));
-// app.use('/api',require("./Routes/OrderData"));
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Food Cart started at port:${PORT}`);
-// });
-
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-// const PORT = process.env.PORT || 5001;
-// const connectToDB = require('./db');
-
-// connectToDB();
-
-// const allowedOrigins = [
-//     'https://food-cart-mern-server-5.onrender.com'
-    
-// ];
-
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// }));
-
-// app.use(express.json());
-// app.use('/api', require("./Routes/CreateUser"));
-// app.use('/api', require("./Routes/DisplayData"));
-// app.use('/api', require("./Routes/OrderData"));
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Food Cart started at port:${PORT}`);
-// });
