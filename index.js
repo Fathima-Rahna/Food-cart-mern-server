79e5c4bf9b04d6a9c91abba7bfa1e2d593f725f6
unknown <rahnaam1997@gmail.com>
2024-05-23 18:26:02 +0530

    saveme

--- index.js
+++ index.js
@@ -2,40 +2,71 @@
 
 
 
+// // const express = require('express');
+// // const cors = require('cors');
+// // const app = express();
+
+// // // const PORT = 5001;
+
+// // const PORT = process.env.PORT || 5001;
+// // const connectToDB = require('./db');
+
+// // connectToDB();
+
+
+// // // app.use(cors());
+
+// // app.use((req, res, next) => {
+// //     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
+// //     res.header(
+// //         "Access-Control-Allow-Headers",
+// //         "Origin, X-Requested-With, Content-Type, Accept"
+// //     );
+// //     next();
+// // })
+
+
+
+
+// // app.use(express.json());
+// // app.use('/api',require("./Routes/CreateUser"));
+// // app.use('/api',require("./Routes/DisplayData"));
+// // app.use('/api',require("./Routes/OrderData"));
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World!');
+// // });
+
+
+// // app.listen(PORT, () => {
+// //   console.log(`Food Cart started at port:${PORT}`);
+// // });
+
+
+
+
+/////////////////////////////////////////////////////////////////
 // const express = require('express');
 // const cors = require('cors');
 // const app = express();
-
-// // const PORT = 5001;
+// require('dotenv').config();
 
 // const PORT = process.env.PORT || 5001;
 // const connectToDB = require('./db');
 
 // connectToDB();
 
-
-// // app.use(cors());
-
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5174");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// })
-
-
-
+// app.use(cors({
+//   origin: 'http://localhost:5173'  // Replace with your actual frontend URL
+// }));
 
 // app.use(express.json());
-// app.use('/api',require("./Routes/CreateUser"));
-// app.use('/api',require("./Routes/DisplayData"));
-// app.use('/api',require("./Routes/OrderData"));
+// app.use('/api', require("./Routes/CreateUser"));
+// app.use('/api', require("./Routes/DisplayData"));
+// app.use('/api', require("./Routes/OrderData"));
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
 // });
-
 
 // app.listen(PORT, () => {
 //   console.log(`Food Cart started at port:${PORT}`);
@@ -44,20 +75,17 @@
 
 
 
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-
 const PORT = process.env.PORT || 5001;
 const connectToDB = require('./db');
 
 connectToDB();
 
-app.use(cors({
-  origin: 'http://localhost:5174'  // Replace with your actual frontend URL
-}));
+// Allow requests from any origin
+app.use(cors());
 
 app.use(express.json());
 app.use('/api', require("./Routes/CreateUser"));
@@ -71,7 +99,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Food Cart started at port:${PORT}`);
 });
-
-
-
-
